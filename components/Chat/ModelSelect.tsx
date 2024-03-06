0e13ec26ae14363ef9fd940cd8cc237780e502f3
HackerGPT <contact@hackergpt.chat>
2024-03-06 15:42:10 -0500

    HackerGPT Pro model

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -172,27 +172,13 @@ export const ModelSelect = () => {
                   onMouseEnter={() => handleModuleEnter(model.name)}
                   onMouseLeave={handleModuleLeave}
                 >
-                  <div className="group/button relative flex w-full items-center justify-center gap-1 rounded-lg py-3 outline-none transition-opacity duration-100 sm:w-auto sm:min-w-[150px] md:gap-2 md:py-3">
+                  <div className="group/button relative flex w-full items-center justify-center gap-1 rounded-lg py-3 outline-none transition-opacity duration-100 sm:w-auto sm:min-w-[170px] md:gap-2 md:py-3">
                     {model.name === 'HackerGPT' ? (
                       <div className="pl-2">
                         <HackerGPTSVG scale={0.1} />
                       </div>
                     ) : (
-                      <div className="pl-2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 16 16"
-                          fill="none"
-                          className="icon-sm group-hover/button:text-brand-purple transition-colors"
-                          width="16"
-                          height="16"
-                        >
-                          <path
-                            d="M12.784 1.442a.8.8 0 0 0-1.569 0l-.191.953a.8.8 0 0 1-.628.628l-.953.19a.8.8 0 0 0 0 1.57l.953.19a.8.8 0 0 1 .628.629l.19.953a.8.8 0 0 0 1.57 0l.19-.953a.8.8 0 0 1 .629-.628l.953-.19a.8.8 0 0 0 0-1.57l-.953-.19a.8.8 0 0 1-.628-.629l-.19-.953h-.002ZM5.559 4.546a.8.8 0 0 0-1.519 0l-.546 1.64a.8.8 0 0 1-.507.507l-1.64.546a.8.8 0 0 0 0 1.519l1.64.547a.8.8 0 0 1 .507.505l.546 1.641a.8.8 0 0 0 1.519 0l.546-1.64a.8.8 0 0 1 .506-.507l1.641-.546a.8.8 0 0 0 0-1.519l-1.64-.546a.8.8 0 0 1-.507-.506L5.56 4.546Zm5.6 6.4a.8.8 0 0 0-1.519 0l-.147.44a.8.8 0 0 1-.505.507l-.441.146a.8.8 0 0 0 0 1.519l.44.146a.8.8 0 0 1 .507.506l.146.441a.8.8 0 0 0 1.519 0l.147-.44a.8.8 0 0 1 .506-.507l.44-.146a.8.8 0 0 0 0-1.519l-.44-.147a.8.8 0 0 1-.507-.505l-.146-.441Z"
-                            fill="currentColor"
-                          ></path>
-                        </svg>
-                      </div>
+                      <HackerGPTSVG scale={0.1} />
                     )}
                     <span className="truncate pr-1.5 text-sm font-medium md:pr-1.5">
                       {model.id === defaultModelId
